2aa309c02c22901e7cba9b9624cf4c3272112555
Josephine Aguilar <josieamor@Josephines-MacBook-Pro.local>
2018-10-07 19:41:28 -0600

    Updates

---
+++ precache-manifest.d72c1adb51c6855cf95a8b4cedaa1a1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "ec92f4e955ee0ce27bac",
+    "url": "/Clicky-Game/static/js/main.ec92f4e9.chunk.js"
+  },
+  {
+    "revision": "8df469f343004280e688",
+    "url": "/Clicky-Game/static/js/1.8df469f3.chunk.js"
+  },
+  {
+    "revision": "ec92f4e955ee0ce27bac",
+    "url": "/Clicky-Game/static/css/main.6ca01d31.chunk.css"
+  },
+  {
+    "revision": "2fd57a3cb9da7b8b0b0c709731169595",
+    "url": "/Clicky-Game/index.html"
+  }
+];
